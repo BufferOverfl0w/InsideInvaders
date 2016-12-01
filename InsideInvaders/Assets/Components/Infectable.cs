0b385e74eed126a1e2e0ba192f62d1478ad3b909
FredericFernandes <fred.fern91@gmail.com>
2016-12-01 15:52:42 +0100

    ajout du prefab pour les lynphosites B et spé
    chaque unitée a canevas.
    Affichage d'une barre de progression pour l'infection

--- InsideInvaders/Assets/Components/Infectable.cs
+++ InsideInvaders/Assets/Components/Infectable.cs
@@ -3,5 +3,6 @@
 public class Infectable : MonoBehaviour {
 	// Advice: FYFY component aims to contain only public members (according to Entity-Component-System paradigm).
 	public bool infecte = false;
-	public int progres_infection = 0;
+	public float progres_infection = 0;
+	public float timeForInfect = 5;  // in sec 
 }
--- InsideInvaders/Assets/Systems/Infection.cs
+++ InsideInvaders/Assets/Systems/Infection.cs
@@ -4,7 +4,7 @@ using FYFY;
 public class Infection : FSystem {
 	private Family _infectableGO = FamilyManager.getFamily(new AllOfComponents(typeof(Infectable)));
 	private Family _infectieuxGO = FamilyManager.getFamily(new AllOfComponents(typeof(Infectieux)));
-	public int seuil_infection = 200;
+
 
 	// Use this to update member variables when system pause. 
 	// Advice: avoid to update your families inside this function.
@@ -20,16 +20,22 @@ public class Infection : FSystem {
 	protected override void onProcess(int familiesUpdateCount) {
 		foreach (GameObject go1 in _infectieuxGO) {
 			float rayon_effet = go1.GetComponent<Infectieux> ().rayon_effet;
-			int pas_infection = go1.GetComponent<Infectieux> ().vitesse_infection;
+			float pas_infection = go1.GetComponent<Infectieux> ().vitesse_infection;
 			Transform tr1 = go1.GetComponent<Transform> ();
+
 			foreach (GameObject go2 in _infectableGO) {
 				Transform tr2 = go2.GetComponent<Transform> ();
 				float distance = Mathf.Sqrt ((tr1.position.x - tr2.position.x) * (tr1.position.x - tr2.position.x)
 				                 + (tr1.position.z - tr2.position.z) * (tr1.position.z - tr2.position.z));
+				
+
+				Infectable inf = go2.GetComponent<Infectable> ();
 				if (distance < rayon_effet) {
-					if (go2.GetComponent<Infectable> ().progres_infection < seuil_infection) {
+					
+					if (inf.progres_infection < inf.timeForInfect) {
 						Debug.Log ("infection en cours");
-						go2.GetComponent<Infectable> ().progres_infection += pas_infection;
+						inf.progres_infection += pas_infection * Time.deltaTime;
+						Debug.Log ("value cell : " + (int)inf.progres_infection);
 					} else {
 						Debug.Log ("infection completee");
 						go2.GetComponent<Infectable> ().infecte = true;
