3f3d6faa3efe450e8d8cd1861640d896c20dc0f2
Cédric Hubert <cedric.hubert13@gmail.com>
2016-12-03 14:31:08 +0100

    Ebauche mission 01
    Mission 01, reste à voir comment afficher briefing début de mission et
    victoire une fois mission accomplie

---
+++ InsideInvaders/Assets/Systems/FinMission01.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using FYFY;
+
+public class FinMission01 : FSystem {
+	private Family _intrusGO = FamilyManager.getFamily(new AllOfComponents(typeof(TeamIntrus)));
+
+	// Use this to update member variables when system pause. 
+	// Advice: avoid to update your families inside this function.
+	protected override void onPause(int currentFrame) {
+	}
+
+	// Use this to update member variables when system resume.
+	// Advice: avoid to update your families inside this function.
+	protected override void onResume(int currentFrame){
+	}
+
+	// Use to process your families.
+	protected override void onProcess(int familiesUpdateCount) {
+		int count = 0;
+		foreach (GameObject go in _intrusGO) {
+			count += 1;
+		}
+		if (count == 0) {
+			Debug.Log ("Mission Accomplie !!!");
+		}
+	}
+}
