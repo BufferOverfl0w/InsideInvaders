1f44d6846747779721832d5f85ba901b62e5b516
wotsale <wotsale@hotmail.com>
2016-12-04 00:38:27 +0100

    maj menus / build

---
+++ InsideInvaders/Assets/Menu Scripts/Menu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	public void NewGame(){
+		Application.LoadLevel ("mission_01");
+	}
+
+	public void SelectMission(){
+		Application.LoadLevel ("menu missions");
+	}
+
+	public void ExitGame(){
+		Application.Quit();
+	}
+}
---
+++ InsideInvaders/Assets/Menu Scripts/MenuMissions.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuMissions : MonoBehaviour {
+
+	public void m1(){
+		Application.LoadLevel ("mission_01");
+	}
+
+	public void m2(){
+		Application.LoadLevel ("mission_02");
+	}
+
+	public void m3(){
+		Application.LoadLevel ("mission_03");
+	}
+
+	public void m4(){
+		Application.LoadLevel ("mission_04");
+	}
+
+	public void m5(){
+		Application.LoadLevel ("mission_05");
+	}
+
+	public void m6(){
+		Application.LoadLevel ("mission_06");
+	}
+
+	public void m7(){
+		Application.LoadLevel ("mission_07");
+	}
+
+	public void m8(){
+		Application.LoadLevel ("mission_08");
+	}
+
+	public void Back(){
+		Application.LoadLevel ("menu");
+	}
+}
