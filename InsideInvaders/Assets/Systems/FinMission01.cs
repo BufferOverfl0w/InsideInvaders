87fb06d8bff9afc0e6fc138be4b3e573bb7a0746
Cédric Hubert <cedric.hubert13@gmail.com>
2016-12-04 18:06:22 +0100

    Gestion victoire/défaite

--- InsideInvaders/Assets/Systems/FinMission01.cs
+++ InsideInvaders/Assets/Systems/FinMission01.cs
@@ -1,8 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using FYFY;
 
 public class FinMission01 : FSystem {
 	private Family _intrusGO = FamilyManager.getFamily(new AllOfComponents(typeof(TeamIntrus)));
+	private Family _defenseGO = FamilyManager.getFamily(new AllOfComponents(typeof(TeamDefense)));
+	private Family _playerGO = FamilyManager.getFamily(new AllOfComponents(typeof(ControllableByKeyboard)));
+
 
 	// Use this to update member variables when system pause. 
 	// Advice: avoid to update your families inside this function.
@@ -17,11 +21,60 @@ public class FinMission01 : FSystem {
 	// Use to process your families.
 	protected override void onProcess(int familiesUpdateCount) {
 		int count = 0;
-		foreach (GameObject go in _intrusGO) {
+		foreach (GameObject intrus in _intrusGO) {
 			count += 1;
 		}
 		if (count == 0) {
 			Debug.Log ("Mission Accomplie !!!");
+			charger_mission_suivante ();
+		} 
+		count = 0;
+		foreach (GameObject player in _playerGO){
+			if (player.GetComponent<BarreDeVie> ().current_pv <= 0) {
+				Debug.Log ("Mission Echouée !!!");
+				recommencer_mission ();
+			}
 		}
+		count = 0;
+		foreach (GameObject defenseur in _defenseGO) {
+			count += 1;
+		}
+		if (count == 0) {
+			Debug.Log ("Mission Echouée !!!");
+			recommencer_mission ();
+		} 
+	}
+
+
+	void charger_mission_suivante(){
+		string scene_name = SceneManager.GetActiveScene().name;
+
+		switch(scene_name){
+			case "mission_01":
+				SceneManager.LoadScene("mission_02",LoadSceneMode.Single);
+				break;
+			case "mission_02":
+				SceneManager.LoadScene("mission_03",LoadSceneMode.Single);
+				break;
+			case "mission_03":
+				SceneManager.LoadScene("mission_04",LoadSceneMode.Single);
+				break;
+			case "mission_04":
+				SceneManager.LoadScene("mission_05",LoadSceneMode.Single);
+				break;
+			case "mission_05":
+				SceneManager.LoadScene("mission_06",LoadSceneMode.Single);
+				break;
+			case "mission_06":
+				SceneManager.LoadScene("mission_07",LoadSceneMode.Single);
+				break;
+			case "mission_07":
+				SceneManager.LoadScene("mission_08",LoadSceneMode.Single);
+				break;
+		}
+	}
+	void recommencer_mission(){
+		string scene_name = SceneManager.GetActiveScene().name;
+		SceneManager.LoadScene(scene_name,LoadSceneMode.Single);
 	}
 }
