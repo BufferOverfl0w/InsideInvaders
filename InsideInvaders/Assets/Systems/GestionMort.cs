9cbf2008b2c2e2a34b7979550396817370da98a8
Cédric Hubert <cedric.hubert13@gmail.com>
2016-12-04 20:22:04 +0100

    dechets mort bactéries

--- InsideInvaders/Assets/Systems/GestionMort.cs
+++ InsideInvaders/Assets/Systems/GestionMort.cs
@@ -5,6 +5,7 @@ public class GestionMort : FSystem {
 	// Use this to update member variables when system pause. 
 	// Advice: avoid to update your families inside this function.
 	private Family _vivantsGO = FamilyManager.getFamily(new AllOfComponents(typeof(BarreDeVie)), new NoneOfComponents(typeof(Infectable)));
+	private Family _bacteries_puresGO = FamilyManager.getFamily(new AnyOfTags("Bacterie"));
 	private Family _vivantsInfectablesGO = FamilyManager.getFamily(new AllOfComponents(typeof(BarreDeVie),typeof(Infectable)));
 	protected override void onPause(int currentFrame) {
 	}
@@ -23,6 +24,7 @@ public class GestionMort : FSystem {
 				GameObjectManager.destroyGameObject(go);
 			}
 		}
+
 		foreach (GameObject go in _vivantsInfectablesGO){
 			if (go.GetComponent<BarreDeVie>().current_pv <= 0){
 				if (go.GetComponent<Infectable> ().infecte == true) {
@@ -32,7 +34,19 @@ public class GestionMort : FSystem {
 					new_go.transform.position = tr.position;
 				} 
 				else {
-					GameObjectManager.destroyGameObject(go);
+					if (go.tag == "Bacterie") {
+						tr = go.GetComponent<Transform> ();
+						GameObjectManager.destroyGameObject (go);
+						GameObject new_go1 = GameObjectManager.instantiatePrefab ("Prefabs/Dechet");
+						GameObject new_go2 = GameObjectManager.instantiatePrefab ("Prefabs/Dechet");
+						GameObject new_go3 = GameObjectManager.instantiatePrefab ("Prefabs/Dechet");
+						new_go1.transform.position = tr.position;
+						new_go2.transform.position = tr.position;
+						new_go3.transform.position = tr.position;
+					} else {
+						GameObjectManager.destroyGameObject(go);
+					}
+
 				}
 
 
